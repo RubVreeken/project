0d435edc35fe0dce8aecbf5dd3848c772370c46d
Kevin Tran <kevintran.1597@gmail.com>
2019-01-17 11:26:53 +0000

    application

--- application.py
+++ application.py
@@ -3,18 +3,14 @@ from flask import Flask, flash, redirect, render_template, request, session, url
 from flask_session import Session
 from passlib.apps import custom_app_context as pwd_context
 from tempfile import mkdtemp
-
+import aiohttp, requests
 import urllib.request
 import json
+from pytrivia import Category, Diffculty, Type, Trivia
 
-
-
-data = json.loads(urllib.request.urlopen("http://jservice.io/api/categories?count=1000").read())
-lijst = []
-for dict in data:
-    lijst.append(dict['id'])
-
-print(lijst)
+my_api = Trivia(True)
+response = my_api.request(1, Category.Books, Diffculty.Hard, Type.Multiple_Choice)
+print(response)
 
 
 
