647eb95b68095cbcc86ecf216b31bcbef0b98052
Kevin Tran <kevintran.1597@gmail.com>
2019-01-18 10:33:34 +0000

    application

--- application.py
+++ application.py
@@ -1,3 +1,5 @@
+# https://github.com/MaT1g3R/Python-Trivia-API
+
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session, url_for
 from flask_session import Session
@@ -8,9 +10,36 @@ import urllib.request
 import json
 from pytrivia import Category, Diffculty, Type, Trivia
 
-my_api = Trivia(True)
-response = my_api.request(1, Category.Books, Diffculty.Hard, Type.Multiple_Choice)
-print(response)
+app = Flask(__name__)
+
+data = Trivia(True)
+response = data.request(2, Category.History ,Diffculty.Hard, Type.Multiple_Choice)
+informatie = []
+
+for info in response['results']:
+    informatie.append(info)
+
+for element in range(len(informatie)):
+    categorie = informatie[element]['category']
+    vraag = informatie[element]['question']
+    goed_antwoord = informatie[element]['correct_answer']
+    foute_antwoorden = informatie[element]['incorrect_answers']
+    moeilijkheidsgraad = informatie[element]['difficulty']
+
+print(categorie, vraag, goed_antwoord, foute_antwoorden, moeilijkheidsgraad)
+
+
+@app.route("/create")
+def create():
+
+    if request.method =="POST":
+
+        return redirect(url_for("index.html"))
+
+    else:
+        # print(categorie)
+        return render_template("create.html", category=categorie)
+
 
 
 
